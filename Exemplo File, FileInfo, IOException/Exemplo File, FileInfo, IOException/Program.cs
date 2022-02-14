28cb65d2c28897290262d42dbcc583e56adbe5aa
Ariel Andrielli <arielandrielli100@gmail.com>
2022-02-14 16:57:13 -0300

    Novos exemplos adicionados - File, FileInfo, IOException - FileStream e StreamReader

---
+++ Exemplo File, FileInfo, IOException/Exemplo File, FileInfo, IOException/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Exemplo_File__FileInfo__IOException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Variables
+
+            string sourcePath = @"C:\Users\aars\Documents\Curso C#\ExemploBanco - Alterações.txt";
+            string targetPath = @"C:\Users\aars\Documents\Curso C#\ExemploBanco - Alterações (teste).txt";
+
+            #endregion
+
+            #region Operations
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                Console.WriteLine("File Successfully Copied!");
+                Console.WriteLine();
+                Console.WriteLine("File Content: ");
+                Console.WriteLine();
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.Write("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+
+            #endregion
+
+        }
+    }
+}
---
+++ Exemplo FileStream e StreamReader/Exemplo FileStream e StreamReader/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exemplo_FileStream_e_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
