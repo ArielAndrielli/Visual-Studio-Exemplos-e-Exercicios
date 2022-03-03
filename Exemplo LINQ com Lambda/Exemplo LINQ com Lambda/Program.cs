989b88dfcc25a858fe090686dda031c3daa839ce
Ariel Andrielli Rodrigues da Silva <arielandrielli100@gmail.com>
2022-03-03 16:58:35 -0300

    Modificacao do Exemplo LINQ com Lambda - Adicionadas Sintaxes Alternativas (sintaxe semelhante a do SQL)

--- Exemplo LINQ com Lambda/Exemplo LINQ com Lambda/Program.cs
+++ Exemplo LINQ com Lambda/Exemplo LINQ com Lambda/Program.cs
@@ -42,45 +42,82 @@ namespace Exemplo_LINQ_com_Lambda
                 new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1 }
                 };
 
-                var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
-                Print("TIER 1 AND PRICE < 900:", r1);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
 
-                var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
-                Print("NAMES OF PRODUCTS FROM TOOLS", r2);
+            //Sintaxe Alternativa (similar à sintaxe do SQL): 
+            var r1 = from p in products
+                     where p.Category.Tier == 1 && p.Price < 900.0
+                     select p;
+                Print("1) - TIER 1 AND PRICE < 900:", r1);
 
-                var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
-                Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
 
-                var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
-                Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+            // Sintaxe Alternativa (similar à sintaxe do SQL)
+            var r2 = from p in products
+                     where p.Category.Name == "Tools"
+                     select p.Name;
+                Print("2) - NAMES OF PRODUCTS FROM TOOLS", r2);
 
-                var r5 = r4.Skip(2).Take(4);
-                Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+
+            //Sintaxe Alternativa:
+
+            var r3 = from p in products
+                     where p.Name.Contains("C")
+                     select new
+                     {
+                         p.Name,
+                         p.Price,
+                         CategoryName = p.Category.Name
+                     };
+
+                Print("3) - NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
+
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+
+            //Sintaxe Alternativa
+
+            var r4 = from p in products
+                     where p.Category.Tier == 1
+                     orderby p.Name
+                     orderby p.Price
+                     select p;
+                Print("4) - TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+
+            //var r5 = r4.Skip(2).Take(4);
+
+            //Sintaxe Alternativa
+
+            var r5 =
+            (from p in r4
+             select p).Skip(2).Take(4);
+
+                Print("5) - TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
 
                 var r6 = products.FirstOrDefault();
-                Console.WriteLine("First or default test1: " + r6);
+                Console.WriteLine("6) - First or default test1: " + r6);
                 var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
-                Console.WriteLine("First or default test2: " + r7);
+                Console.WriteLine("7) - First or default test2: " + r7);
                 Console.WriteLine();
 
                 var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
-                Console.WriteLine("Single or default test1: " + r8);
+                Console.WriteLine("8) - Single or default test1: " + r8);
                 var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
-                Console.WriteLine("Single or default test2: " + r9);
+                Console.WriteLine("9) - Single or default test2: " + r9);
                 Console.WriteLine();
 
                 var r10 = products.Max(p => p.Price);
-                Console.WriteLine("Max price: " + r10);
+                Console.WriteLine("10) - Max price: " + r10);
                 var r11 = products.Min(p => p.Price);
-                Console.WriteLine("Min price: " + r11);
+                Console.WriteLine("11) - Min price: " + r11);
                 var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
-                Console.WriteLine("Category 1 Sum prices: " + r12);
+                Console.WriteLine("12) - Category 1 Sum prices: " + r12);
                 var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
-                Console.WriteLine("Category 1 Average prices: " + r13);
+                Console.WriteLine("13) - Category 1 Average prices: " + r13);
                 var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
-                Console.WriteLine("Category 5 Average prices: " + r14);
+                Console.WriteLine("14) - Category 5 Average prices: " + r14);
                 var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
-                Console.WriteLine("Category 1 aggregate sum: " + r15);
+                Console.WriteLine("15) - Category 1 aggregate sum: " + r15);
                 Console.WriteLine();
 
                 var r16 = products.GroupBy(p => p.Category);
